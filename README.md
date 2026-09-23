ff94a34ccf15332a1a301e73080097445d2e578c
Chris Funke <cfunke@intevation.de>
2026-09-23 15:06:35 +0200

    feat: Pull in all directories within registries
    Display different registries as headings

--- README.md
+++ README.md
@@ -8,26 +8,22 @@ SPDX-License-Identifier: Apache-2.0
 To build the static website you will need
 - [hugo](https://gohugo.io)
 - [python](https://python.org)
-- [python-requests](https://pypi.org/project/requests/)
+- [pygithub](https://pypi.org/project/PyGithub/)
 - [git](https://git-scm.com)
 
-
 ## Building
-
 After cloning the repository, run the following command to pull the submodules:
 
 ```sh
 git submodule update --init --recursive
 ```
 
-To grab the updated source files run
+To download the registry sources run
 ```sh
 ./get.py
 ```
 
-
 ### Development
-
 Finally you can either run the live updating page locally (for development purposes) by running
 ```sh
 hugo serve -D
@@ -35,7 +31,6 @@ hugo serve -D
 This will also run a web server serving the resulting page on [localhost:1313](https://localhost:1313).
 
 ### Production
-
 Or you can generate the HTML files for the purpose of hosting it somewhere by running
 ```sh
 hugo build --minify --baseURL https://example.net
--- get.py
+++ get.py
@@ -4,15 +4,34 @@
 
 import json
 import os
+from pathlib import Path
 
-import requests
+from github import Github, Repository
 
-r = requests.get('https://github.com/oasis-tcs/csaf/raw/refs/heads/master/registry/id/mapping.json')
-data = r.json()
-os.makedirs('data', exist_ok=True)
-with open('data/mapping.json', 'w', newline='\n') as f:
-    json.dump(data, f, indent=2)
-r = requests.get('https://github.com/oasis-tcs/csaf/raw/refs/heads/master/registry/id/registry.json')
-data = r.json()
-with open('data/registry.json', 'w', newline='\n') as f:
-    json.dump(data, f, indent=2)
+
+def load_registry(repo: Repository.Repository, path: str):
+    res = {}
+    for entry in repo.get_contents(path):
+        if entry.type == 'file' and (entry.name == 'mapping.json' or entry.name == 'registry.json'):
+            res[Path(entry.name).stem] = json.loads(entry.decoded_content.decode('utf-8'))
+    return res
+
+def main():
+    gh = Github(lazy=True)
+    repo = gh.get_repo('oasis-tcs/csaf')
+    registries = {'mapping': {}, 'registry': {}}
+
+    for entry in repo.get_contents('registry'):
+        if entry.type == 'dir':
+            print(f'Downloading Registry {entry.name} from {repo.owner.login}/{repo.name}')
+            loaded = load_registry(repo, f'registry/{entry.name}')
+            if 'mapping' in loaded:
+                registries['mapping'][entry.name] = loaded['mapping']
+            if 'registry' in loaded:
+                registries['registry'][entry.name] = loaded['registry']
+    os.makedirs('data', exist_ok=True)
+    with open('data/registries.json', 'w') as f:
+        json.dump(registries, f, indent=2)
+
+if __name__ == '__main__':
+    main()
